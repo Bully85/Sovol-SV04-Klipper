154ba22f8d46c619f70477e8d2e46047a3cbd7d9
Andreas Bulling <andy.bulling@gmx.de>
2023-05-12 01:26:30 +0200

    fix adxl

--- config/macro/shell_commands/adxl_x.sh
+++ config/macro/shell_commands/adxl_x.sh
@@ -1,1 +1,1 @@
-~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o ~/klipper_config/shaper_calibrate_x.png
+~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o ~/printer_data/config/shaper_calibrate_x.png
--- config/macro/shell_commands/adxl_y.sh
+++ config/macro/shell_commands/adxl_y.sh
@@ -1,1 +1,1 @@
-~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_y_*.csv -o ~/klipper_config/shaper_calibrate_y.png
+~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_y_*.csv -o ~/printer_data/config/shaper_calibrate_y.png
