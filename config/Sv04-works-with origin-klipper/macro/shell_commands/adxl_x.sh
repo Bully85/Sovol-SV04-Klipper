ff161d0487f2140713cafc5ff3e1690ca5dcef93
Bully85 <andy.bulling@gmx.de>
2023-09-22 01:31:51 +0200

    new config

---
+++ config/Sv04-works-with origin-klipper/macro/shell_commands/adxl_x.sh
@@ -0,0 +1,1 @@
+~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o ~/printer_data/config/shaper_calibrate_x.png
---
+++ config/Sv04-works-with origin-klipper/macro/shell_commands/adxl_y.sh
@@ -0,0 +1,1 @@
+~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_y_*.csv -o ~/printer_data/config/shaper_calibrate_y.png
